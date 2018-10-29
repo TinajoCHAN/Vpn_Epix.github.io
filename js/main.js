d3cbdc4c28f17a1bfc01b5aa50fe6f8cd64fd2bd
TinajoCHAN <44139540+TinajoCHAN@users.noreply.github.com>
2018-10-29 19:05:06 +0000

    new page
    its new!

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function playAudio() {
+    console.log("dabn");
+    document.getElementById("my_audio").play();
+}
+
+
+function log() {
+    var x = document.getElementById("1").value;
+    var y = document.getElementById("2").value;
+    var z = y;
+    var k = x;
+    if ((z == '77777' && k == 'javi77777') || (z == '2' && k == 'tobiasid')) {
+        window.location.replace("accesed.html");
+    } else {
+
+        $('#panel').addClass('shaky');
+    }
+}
+
+
+$(document).ready(function () {
+    $("#dank").click(function () {
+        $("#panel").slideDown("slow");
+        $("#dank").fadeOut("slow");
+        $('.anime, .anime2').slideDown("slow").fadeIn("slow", function () {});
+    });
+});
